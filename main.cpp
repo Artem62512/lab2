c265e7609e433f91c81f9e1661e564144d8bb2ae
Artem62512 <144590995+Artem62512@users.noreply.github.com>
2023-11-05 12:11:21 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+
+// picture 2
+
+using namespace std;
+
+float line (int x)
+{
+    float y=0;
+    if(x >= -10 && x < -8)
+    {
+        y = -3;
+    }
+    if (x >= -8 && x < -3)
+    { 
+        y = (x+3)*(0.6);
+    }
+    if (x >= -3 && x < 3)
+    {
+        y = sqrt (9 - x * x);
+    }
+    if (x >= 3 && x <= 5)
+    {
+        y = x - 3;
+    } 
+    if (x > 5 && x <=8)
+    {
+        y = 3;
+    }
+    return y;
+}
+
+float main()
+{
+    cout << "START" << endl;
+    for ( int x = - 10; x <= 8; x++ )
+    {
+        cout << "x = " << x << "   y = " << line (x) << endl;
+
+    }
+    cout << "END";
+}
+
